426c78ed0451eb84934c16415c37c6469eeb332d
Challa Saraswathi Lalith <121499856+lalithcs@users.noreply.github.com>
2023-05-31 14:37:30 +0530

    Update register.php database connect 2

--- register.php
+++ register.php
@@ -1,13 +1,13 @@
 <?php
-$host="localhost";
-$port=3306;
-$socket="";
-$user="root";
-$password="";
-$dbname="sys";
+$host = "localhost";
+$port = 3306;
+$socket = "";
+$user = "root";
+$password = "";
+$dbname = "users";
 
-$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+// Create a new mysqli connection
+$conn = new mysqli($host, $user, $password, $dbname, $port, $socket);
 
 // Check connection
 if ($conn->connect_error) {
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $passwordConfirmation = $_POST["password_confirmation"];
 
-    // Validate input data (similar to previous code)
+    // Validate input data
     if (empty($username) || empty($password) || empty($passwordConfirmation)) {
         echo "Please fill in all fields.";
         exit();
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Check if username already exists
-    $query = "SELECT * FROM users WHERE username = '$username'";
+    $query = "SELECT * FROM users.users WHERE username = '$username'";
     $result = $conn->query($query);
 
     if ($result->num_rows > 0) {
@@ -40,21 +40,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 
-    // Hash the password (similar to previous code)
+    // Hash the password
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
     // Insert the user into the database
-    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$hashedPassword')";
+    $sql = "INSERT INTO users.users (username, password) VALUES ('$username', '$hashedPassword')";
 
-    if ($conn->query($sql) === TRUE) 
-    {
+    if ($conn->query($sql) === TRUE) {
         header("Location: login.html");
         exit;
-    }
-     else {
+    } else {
         echo "Error: " . $conn->error;
     }
 }
+
 // Close database connection
 $conn->close();
 ?>
